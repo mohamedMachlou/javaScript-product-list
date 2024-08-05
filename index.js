d849bf13a70d1b8c9570c95a6cbfdb6e859ff6ca
Mohamed Machlou <medmachlou@gmail.com>
2024-08-06 00:13:20 +0100

    Moins and Plus Counter EventListener

--- index.js
+++ index.js
@@ -8,24 +8,22 @@ const allProducts = [
     name: "Waffle",
     title: "Xaffle with Berries",
     price: 6.5,
-    counter: 0,
+    counter: 1,
     img: "./assets/images/image-waffle-desktop.jpg",
   },
   {
     name: "Créme Brulée",
     title: "Vanilla Bean Créme Brulée",
     price: 7.99,
-    counter: 0,
+    counter: 1,
   },
   {
     name: "Macaron",
     title: "Macaron Mix of Five",
     price: 8.46,
-    counter: 0,
+    counter: 1,
   },
 ];
-
-//
 
 // Hide product Counter from product
 productCount.map((ele) => {
@@ -37,48 +35,44 @@ addToCart.map((ele, index) => {
   ele.addEventListener("click", (e) => {
     ele.setAttribute("style", "display: none");
     productCount[index].setAttribute("style", "display: flex");
+    productCount[index].children[1].textContent = allProducts[index].counter;
+    console.log(productCount[index].children[1].textContent);
     products[index].children[0].children[0].classList.add("prdactive");
   });
 });
 
-//
+// Set Name, Title and Price From All products Array to Display
 products.map((product, index) => {
   product.children[1].textContent = allProducts[index].name;
   product.children[2].textContent = allProducts[index].title;
   product.children[3].textContent = `$${allProducts[index].price}`;
-  //   console.log(product.children[1]);
-  //   console.log(product.children[2]);
-  //   console.log(product.children[3]);
 });
 
 //
-const productCnts = Array.from(document.querySelectorAll(".product-count"));
-
-function moinsCounter() {
-  productCnts.map((prd, index) => {
-    prd.children[0].addEventListener("click", () => {
-      if (+prd.children[1].innerHTML >= 1) {
-        +prd.children[1].innerHTML--;
-        console.log(prd.children[0].parentElement.parentElement);
-        if (+prd.children[1].innerHTML == 0) {
-          prd.children[0].parentElement.parentElement.children[0].classList.remove(
-            "prdactive"
-          );
-          prd.children[0].parentElement.parentElement.children[1].setAttribute(
-            "style",
-            "display: flex"
-          );
-          prd.children[0].parentElement.parentElement.children[1].setAttribute(
-            "style",
-            "display: flex"
-          );
-        }
-      }
-      console.log("index : ", +prd.children[1].innerHTML);
-    });
+const moins = Array.from(document.querySelectorAll(".moins"));
+moins.forEach((m, index) => {
+  m.addEventListener("click", () => {
+    let counter = +m.parentElement.children[1].textContent;
+    if (counter >= 2) {
+      counter--;
+    }
+    if (counter == 1) {
+      productCount[index].setAttribute("style", "display: none");
+      addToCart[index].setAttribute("style", "display: flex");
+      products[index].children[0].children[0].classList.remove("prdactive");
+    }
+    m.parentElement.children[1].textContent = counter;
+    allProducts[index].counter = counter;
   });
-}
+});
 
-function plusCounter() {
-  console.log("plus");
-}
+//
+const plus = Array.from(document.querySelectorAll(".plus"));
+plus.forEach((p, index) => {
+  p.addEventListener("click", () => {
+    let counter = +p.parentElement.children[1].textContent;
+    counter++;
+    p.parentElement.children[1].textContent = counter;
+    allProducts[index].counter = counter;
+  });
+});
